9aa016f55c6cb8b90092c59509285cd5d1c7db4d
rluca <rluca6448@gmai>
2024-02-19 09:47:01 -0300

    Refactored and fixed some bugs

--- src/main.py
+++ src/main.py
@@ -1,33 +1,46 @@
 import random
 from functions import *
 
-words = {
-    3: ["uva", "uso", "oso", "dos"],
-    4: ["toro", "casa", "sapo"],
-    5: ["cinco", "siete"],
-}
+words = process_words()
+min_letters = min(words.keys())
+max_letters = max(words.keys())
 
-while True:
-    length = int(input("Con cuantas letras queres jugar?\n"))
-    if min(words.keys()) <= length <= max(words.keys()):
-        break
+wanna_play = True
 
-attempt = int(input("Cuantos intentos te gustaria tener?\n"))
-
-word = random.choice(words[length])
-
-table = make_table(length, attempt)
-
-not_guessed = True
-
-while not_guessed and attempt > 0:
+while wanna_play:
     while True:
-        guess = input(f"Input a word with {len(word)} characters: \n")
-        if len(guess) == length:
+        length = int(input(f"Con cuantas letras queres jugar ({min_letters}-{max_letters})?\n"))
+        if min_letters <= length <= max_letters:
             break
-    not_guessed = check_table(table, guess, word, attempt, length)
-    attempt -= 1
-    if attempt == 0:
+
+    attempt = 8
+
+    word = random.choice(words[length])
+
+    table = make_table(length, attempt)
+
+    not_guessed = True
+
+    while not_guessed and attempt > 0:
+        while True:
+            guess = input(f"Input a word with {len(word)} characters: \n")
+            if len(guess) != length:
+                print("Please input a guess of valid length!")
+            elif guess.lower() not in words[length]:
+                print("That word is not available!")
+            else:
+                break
+        not_guessed = check_table(table, guess, word, attempt, length)
+        attempt -= 1
+
+    if not_guessed:
         print("You lost! No more attempts for you :(")
-        exit()
-print("Congrats! You won at the attempt number", len(table) - attempt)
+        print(f"The correct word was {word}!")
+    else:
+        print("Congrats! You won at the attempt number", len(table) - attempt)
+
+    again = play_again()
+    if again == 1:
+        continue
+    else:
+        break
