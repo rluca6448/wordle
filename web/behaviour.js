0b6db8c868256ed2e96d0141b0bd499df498f352
rluca <rluca6448@gmai>
2024-02-19 09:49:44 -0300

    Added resources

---
+++ web/behaviour.js
@@ -0,0 +1,6 @@
+function toggle() {
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup = document.getElementById('menu');
+    popup.classList.toggle('active')
+}
